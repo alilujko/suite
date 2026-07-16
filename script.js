3d313b60a115da3ce4785cf5f87cde2c388a06ab
alilujko <u.alilujko@gmail.com>
2026-07-16 18:06:01 +0300

    six-seven

---
+++ script.js
@@ -0,0 +1,10 @@
+while (true) {
+    const name = prompt("Ведіть пароль від сайту")
+
+    if (name == "67") {
+        alert("Пароль прийнято")
+        brake
+    } else {
+        alert("Хибний пароль")
+    }
+}
